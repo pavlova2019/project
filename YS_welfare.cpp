73b977cb9d42bf329c47e20fe744f4a1acef0dfb
Pavlova Alexandra <54851676+pavlova2019@users.noreply.github.com>
2020-09-16 23:00:51 +0300

    Bad Strategic Behavior in One-Sided Matching

--- YS_welfare.cpp
+++ YS_welfare.cpp
@@ -5,20 +5,23 @@
 
 using namespace std;
 
-int p[n][n] = {
+int p[n][n] = { // we are studying this particular profile
 {1, 2, 3, 4},
 {1, 2, 3, 4}, 
 {1, 3, 2, 4},
 {1, 4, 3, 2}
 };
 
-int now[n], util[n], ban[n][n];
+int now[n]; // item i belings to agent now[i-1]
+int util[n]; // agent i has util[i-1] points
+int ban[n][n]; // ban[i][j] = 1, if agent i can't steal item j no more
+int order[n], used[n];
 
 int get_next(int num){
 	int st = 0;
 	for (int i=0;i<n;i++) if (!ban[num-1][p[num-1][i]-1]) {
-		int item = p[num-1][i]-1;
-		if (now[item]) {
+		int item = p[num-1][i]-1; // first most valuable item available
+		if (now[item]) { // means that some other agent has this item, so we steal
 			st = now[item];
 			util[st-1] = 0;
 			ban[st-1][item] = 1;
@@ -34,7 +37,7 @@ int get_next_2(int num){
 	int st = 0, f = 0, it = 0;
 	for (int i=0;i<n;i++) if (!ban[num-1][p[num-1][i]-1]) {
 		if (f){
-			int item = p[num-1][i]-1;
+			int item = p[num-1][i]-1; // second most valuable item available
 			if (now[item]) {
 				st = now[item];
 				util[st-1] = 0;
@@ -48,7 +51,7 @@ int get_next_2(int num){
 		f = p[num-1][i];
 		it = i;
 	}
-	if (f){
+	if (f){ // if we didn't find the second most valuable item, we take the first
 		int item = f - 1;
 		if (now[item]) {
 			st = now[item];
@@ -61,18 +64,16 @@ int get_next_2(int num){
 	return st;
 }
 
-int order[n], used[n];
-
-void YS(int from, int it){
+void YS(int from, int it){ //from = 0 (if we didn't steal during the previous round), id of agent we stole from -- otherwise
 	if (it == n) return;
-	int num = order[it];
-	if (from) num = from;
-	int nxt = get_next(num);
+	int num = order[it]; // next agent
+	if (from) num = from; // agent we stoli from (if we did)
+	int nxt = get_next(num); // next agent or 0
 	if (nxt) YS(nxt, it);
 	else YS(0, it+1);
 }
 
-void S2(int from, int it){
+void S2(int from, int it){ // the same as YS
 	if (it == n) return;
 	int num = order[it];
 	if (from) num = from;
@@ -90,7 +91,7 @@ void init(bool f){
 }
 
 int main() {
-	set <int> s;
+	set <int> s; // set of numbers like "1234"
 	double ys = 0, s2 = 0;
 	while (s.size() < 24){
 		init(1);
@@ -110,11 +111,11 @@ int main() {
 		}
 		else continue;
 		YS(0, 0);
-		int sum = 0;
+		int sum = 0; // total utility for the given order, YS
 		for (int i=0;i<n;i++) sum += util[i];
 		ys += (sum + 0.0);
 		init(0);
-		S2(0, 0);
+		S2(0, 0); // total utility for the given order, YS + our strategy
 		int sum_2 = 0;
 		for (int i=0;i<n;i++) sum_2 += util[i];
 		s2 += (sum_2 + 0.0);
